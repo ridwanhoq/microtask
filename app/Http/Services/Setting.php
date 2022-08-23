c6cdeb89019fce791af77d3046c80015f998b5bc
ridwanul <ridwanul.riseuplabs@gmail.com>
2022-08-23 14:44:45 +0600

    Created: Setting arrays added

---
+++ app/Http/Services/Setting.php
@@ -0,0 +1,245 @@
+<?php
+
+namespace App\Http\Services;
+
+class Setting{
+
+
+    public static function heights_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+
+    public static function complexions_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+
+    public static function healths_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+
+    public static function blood_groups_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+    
+    public static function marital_statuses_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+    
+    public static function religions_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+
+    public static function subjects_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+
+    public static function degrees_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+    
+    public static function institutions_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+
+    public static function years_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+
+    public static function professions_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+    
+    public static function designations_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+    public static function residences_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+    public static function prayers_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+
+    public static function fastings_array(){
+        return [
+            1   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            2   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+            3   => [    'en'    => 'Below 4ft',      'bn'    => '৪ ফুট এর কম'],
+        ];
+    }
+
+
+    
+
+
+
+
+
+
+    
+
+
+/**
+ * below methods are to manage all arrays 
+ */
+
+
+    public static function array_list_by_element_index( $staticfunction, $index = 'en' ) {
+        // for religions
+        // self::array_list_by_element_key('religions', 'en');
+        $out_array = [];
+        foreach ( self::$staticfunction() as $key => $array ) {
+            $out_array[$key] = $array[$index];
+        }
+        return $out_array;
+    }
+
+    public static function get_data_by_index( $staticfunction, $index, $lan = 'en', $extra_title = null ) {
+        $found = in_array( $index, array_keys( self::$staticfunction() ) );
+
+        if ( !$found ) {
+            return null;
+        }
+        $title_lan = $extra_title . '_' . $lan;
+        return $extra_title != null ? self::$staticfunction()[$index][$title_lan] : self::$staticfunction()[$index][$lan];
+    }
+
+    public static function get_other_data_by_index( $staticfunction, $index, $lan, $extra_titles = [] ) {
+        $data = [];
+
+        if ( empty( $extra_titles ) ) {
+            return $data;
+        }
+
+        foreach ( $extra_titles as $key => $extra_title ) {
+            $title_value = self::get_data_by_index( $staticfunction, $index, $lan, $extra_title );
+
+            $data[$key] = $title_value;
+        }
+
+        return $data;
+    }
+
+    public static function check_keys_exists( $staticfunctions = [], $indices = [] ) {
+        $fields = [];
+        if ( empty( $staticfunctions ) || empty( $indices ) ) {
+            return false;
+        }
+
+        if ( is_array( $staticfunctions ) ) {
+
+            foreach ( $staticfunctions as $key => $fnc ) {
+
+                $get_field = self::check_key_exists( $fnc, $indices[$key] );
+
+                if ( $get_field != "" ) {
+                    $fields[$get_field] = $get_field;
+                }
+
+            }
+        }
+
+        return $fields;
+    }
+
+    public static function check_key_exists( $staticfunction, $index ) {
+        $key_exists = array_key_exists( request()->$index, self::$staticfunction() );
+        $field      = "";
+
+        if ( !$key_exists ) {
+            $field = $index;
+        }
+
+        return $field;
+    }
+
+    public static function get_array_that_return_only_key( $staticfunction, $index = 'key' ) {
+
+        $out_array = [];
+        foreach ( self::$staticfunction() as $key => $array ) {
+            array_push( $out_array, $array[$index] );
+        }
+        return $out_array;
+    }
+
+    // Array Search With Specific Cloumn Value
+    public static function searchWithColumn( $staticfunction, $coloumn, $value ) {
+        $data = self::$staticfunction();
+
+        $key = array_search( $value, array_column( $data, $coloumn ) );
+        return $data[$key + 1];
+    }
+
+
+
+}
+
